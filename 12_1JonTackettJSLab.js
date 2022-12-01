019a0cc811c45d13bf68390ddbb8302aafa65935
JonCTack <jon.c.tackett@gmail.com>
2022-12-01 14:26:50 -0600

    seventh commit, before cats

--- 12_1JonTackettJSLab.js
+++ 12_1JonTackettJSLab.js
@@ -244,8 +244,45 @@ const printLongestWord = (stringArray) =>{
 user = {
   name: "tom",
   email: "tom@tom.com",
-  age: "66",
+  age: 66,
   purchased: []
 }
 //B.
-
+user.email = "tom@gmail.com"
+user.age++
+//C.
+user.location = "townsburg"
+//D.
+user.purchased.push("carbohydrates")
+user.purchased.push("peace of mind")
+user.purchased.push("Merino jodhpurs")
+console.log(user.purchased[2])
+//E.
+user.friend = {
+  name: "Grace Hopper",
+  age: 85,
+  location: "townsburg",
+  purchased: []
+}
+console.log(user.friend.name)
+console.log(user.friend.location)
+user.friend.age = 55
+user.friend.purchased.push("The One Ring")
+user.friend.purchased.push("A latte")
+console.log(user.friend.purchased[1])
+//F.
+for (let i = 0; i < user.purchased.length; i++){
+  console.log(user.purchased[i])
+}
+for (let i = 0; i < user.friend.purchased.length; i++){
+  console.log(user.friend.purchased[i])
+}
+const updateUser = () => {
+  user.age++
+  user.name.toUpperCase()
+}
+const oldAndLoud = (person) => {
+  person.age++;
+  person.name = person.name.toUpperCase();
+}
+oldAndLoud(user)
